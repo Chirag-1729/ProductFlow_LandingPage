5df3c19fb8c1155a1ad80b6c0735606dded2ed55
Chirag-1729 <chiragsingh1729@gmail.com>
2025-06-29 00:36:55 +0530

    Initial commit - Responsive Landing Page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
               onClick={handleSignIn}
               className="text-gray-600 hover:text-gray-900 transition-colors"
             >
-              Sign In
+              Sign Up
             </button>
             <button 
               onClick={handleGetStarted}
